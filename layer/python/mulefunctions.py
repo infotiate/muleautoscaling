674ebbce51d578357cf0119224d76c8ee3111dc5
Infotiate <info@infotiate.com>
2020-01-29 21:44:36 +0000

    Added support for Mule Connected Apps

--- layer/python/mulefunctions.py
+++ layer/python/mulefunctions.py
@@ -13,6 +13,10 @@ LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
 def lambda_getArmAccessToken(event):
+    return lambda_getAccessTokenFromConnecedApp(event)
+
+
+def lambda_getArmAccessTokenFromUser(event):
     # TODO implement
   # Use this code snippet in your app.
 # If you need more information about configurations or implementing the sample code, visit the AWS docs:   
@@ -109,6 +113,107 @@ def lambda_getArmAccessToken(event):
         'orgId': orgId 
     }
 
+
+def lambda_getAccessTokenFromConnecedApp(event):
+    # TODO implement
+  # Use this code snippet in your app.
+# If you need more information about configurations or implementing the sample code, visit the AWS docs:   
+# https://aws.amazon.com/developers/getting-started/python/
+
+    print (event)
+
+    secret_name = "MuleAnypointUser"
+    region_name =  event['region']
+    tokenType = event.get('tokenType',"")
+    
+    secret = ""
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({ "error":"Error processing your request " + e.response["Error"]["Code"]})
+        }
+     
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+     
+    if (len(secret) <= 0) :
+        return {
+            'statusCode': 400,
+            'body': json.dumps('{ "error":"Bad request"}')
+        }
+    
+    p = json.loads(secret)
+    domain = p['muleDomainUrl']
+    envId = p['envId']
+    orgId = p['orgId']
+    client_id = p['client_id']
+    client_secret = p['client_secret']
+    
+    conn = http.client.HTTPSConnection(domain)
+    
+    #Obtain Access token
+    
+    payload = {'client_id': client_id, 
+                'client_secret': client_secret,
+                'grant_type' : 'client_credentials',
+                'scopes': 'full'
+    }
+    rsp = requests.post("https://anypoint.mulesoft.com/accounts/api/v2/oauth2/token", data=payload)
+    print(rsp.json())
+    anypoint_access_token = rsp.json()['access_token']
+    
+    print("Access token",anypoint_access_token)
+    
+    if (tokenType == 'access') :
+        #Return the registration response
+        return {
+            'statusCode': 200,
+            'access_token': anypoint_access_token,
+            'envId': envId,
+            'orgId': orgId 
+        }
+    
+    #Call Runtime Manager API with access token obtained in the previous call 
+    #to obtain registration token
+    
+    armrq_headers = {
+        'x-anypnt-env-id': envId, 
+        'x-anypnt-org-id': orgId,
+        'Authorization': 'Bearer ' + anypoint_access_token
+    }
+       
+    
+    armReqUrl = '/hybrid/api/v1/servers/registrationToken'
+    conn.request('GET', armReqUrl, "", armrq_headers)
+    arm_response = json.loads(conn.getresponse().read().decode())['data']
+    
+    
+    #Return the registration response
+    return {
+        'statusCode': 200,
+        'registration_token': json.dumps(arm_response),
+        'access_token': anypoint_access_token,
+        'envId': envId,
+        'orgId': orgId 
+    }        
+    
+    
 #Deploy application from mule application file    
 def deployAppFromFile(event, appName, targetId, applicationFile):
     armrq_headers = {
